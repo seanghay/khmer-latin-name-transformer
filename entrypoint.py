0db7744db5faf4a1728167062d311b73d800454c
Seanghay Yath <seanghay.dev@gmail.com>
2023-10-17 18:50:11 +0700

    fix: safe wrapper

--- entrypoint.py
+++ entrypoint.py
@@ -3,8 +3,28 @@ from pathlib import Path
 from argparse import ArgumentParser
 from khmer_latin_name_transformer import to_khmer, to_khmer_pronounce, to_latin
 
+def safe_to_pronouce(input):
+    try:
+        return to_khmer_pronounce(input)
+    except:
+        return None
+
+def safe_to_latin(input):
+    try:
+        return to_latin(input)
+    except:
+        return None
+    
+def safe_to_khmer(input):
+    try:
+        return to_khmer(input)
+    except:
+        return None
+    
+
+
 if __name__ == "__main__":
-    parser = ArgumentParser("transform.py", description="Transform Text to Text")
+    parser = ArgumentParser("entrypoint.py", description="Transform Text to Text")
     parser.add_argument(
         "files", nargs="+", type=Path, default=[], help="Path to input file"
     )
@@ -12,13 +32,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "-j", "--job", type=int, default=mp.cpu_count(), help="Number of processors"
     )
-    
+
     args = parser.parse_args()
+    
     transformers = {
-      "khmer": to_khmer,
-      "latin": to_latin,
-      "pro": to_khmer_pronounce,  
-    }  
+        "khmer": safe_to_khmer,
+        "latin": safe_to_latin,
+        "pro": safe_to_pronouce,
+    }
+    
     with mp.Pool(args.job) as pool:
         for file in args.files:
             with open(file) as infile:
